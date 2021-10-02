a56c5dc710920579df290a3208511ef5568fe3ba
Sabrina Reis <62216740+Saccha@users.noreply.github.com>
2021-10-01 22:49:27 -0300

    Update Questões.kt

--- AC02_Kotlin/Questões.kt
+++ AC02_Kotlin/Questões.kt
@@ -10,3 +10,26 @@ class Aluno(id: Int, nome: String, curso:String) {
 fun main() {
 	var obj = Aluno(1, "Fulano", "Análise e Desenvolvimento de Sistemas")
 }
+
+/*---------------------------------------------------------------------------------*/
+
+/*3. A classe Aluno possuí os atributos id, nome e curso. Receba todos os
+parâmetros no método construtor e exiba-os em console. Adicione um
+companion Object com a mensagem chamando método estático, que deve ser
+acessado sem realizar a instância direta da classe.*/
+
+class Aluno(id: Int, nome: String, curso:String) {
+	companion object Factory {
+    	fun chamar(){
+      		println("Chamando método estático")
+    	}
+	}
+	init {
+   		println("ID: $id \nNome: $nome\nCurso:$curso")
+	}
+}
+
+fun main() {
+    var obj = Aluno(1, "Fulano", "Análise e Desenvolvimento de Sistemas")
+    Aluno.chamar()
+
